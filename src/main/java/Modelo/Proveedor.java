5dd956a8ae4258c8e8a4f5656a8ff1f48de0e1cd
slvnvc <silvanavillanuevacap@gmail.com>
2024-10-23 12:43:16 -0500

    miercolesdeparo

---
+++ src/main/java/Modelo/Proveedor.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Silvana Villanueva
+ */
+public class Proveedor {
+     private int idProveedor;
+    private String Nombre;
+    private String Telefono;
+    private String Direccion;
+
+    public Proveedor(int idProveedor, String Nombre, String Telefono, String Direccion) {
+        this.idProveedor = idProveedor;
+        this.Nombre = Nombre;
+        this.Telefono = Telefono;
+        this.Direccion = Direccion;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
+    
+}
